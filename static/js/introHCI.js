93a765062841a1048b0382e55e6a9d6297e9cc59
Walker Carlson <wcarlson@ucsd.edu>
2019-01-22 10:21:20 -0800

    made change

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,23 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+	$("#testjs").click(function(e){
+				$('.jumbotron h1').text("JS is connected")
+		});
+
+		$("a.thumbnail").click(projectClick);
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
+	function projectClick(e) { 
+	    // prevent the page from reloading      
+	    e.preventDefault();
+	    // In an event handler, $(this) refers to      
+	    // the object that triggered the event      
+	    $(this).css("background-color", "#7fff00");
+	     var containingProject = $(this).closest(".project");
+	        containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	}
